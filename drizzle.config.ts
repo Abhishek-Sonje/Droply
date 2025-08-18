65caa8e7bee674ad860f3b7ca68335d6423d91ec
Abhishek Sonje <code.abhishek03@gmail.com>
2025-08-19 01:47:37 +0530

    Completed Schema and db linking

---
+++ drizzle.config.ts
@@ -0,0 +1,28 @@
+import * as dotenv from "dotenv"
+// import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+dotenv.config({ path: ".env.local" });
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("DatabaseURL is not set")
+}
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./lib/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    },
+    migrations: {
+        table: "__drizzle_migration",
+        schema:"public"
+    },
+    verbose: true,
+    strict:true
+});
+
+
+
+ 
---
+++ lib/db/schema.ts
@@ -0,0 +1,48 @@
+import {
+  pgTable,
+  text,
+  uuid,
+  integer,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+export const files = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  //BASIC FILE INFO
+  name: text("name").notNull(),
+  path: text("path").notNull(), // /document/file/resume
+  type: text("type").notNull(),
+  size: integer("size").notNull(),
+
+  //STORAGE INFO
+  fileUrl: text("file_url").notNull(), // Url to access file
+  thumbnailUrl: text("thumbnail_url"),
+
+  //OWNERSHIP
+  userId: text("user_id").notNull(),
+  parentId: text("parent_id"), // Parent folder id (null for root folder)
+
+  //FILE/FOLDER FLAGS
+  isFolder: boolean("is_folder").default(false).notNull(),
+  isStarred: boolean("is_starred").default(false).notNull(),
+  isTrash: boolean("is_trash").default(false).notNull(),
+
+  //TIMESTAMP
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const filesRelations = relations(files, ({ one, many }) => ({
+  parent: one(files, {
+    fields: [files.parentId],
+    references: [files.id],
+  }),
+
+  children: many(files),
+}));
+
+export const File = typeof files.$inferSelect;
+export const NewFile = typeof files.$inferInsert;
