8e2c82b522bb3c948e92b0d8be9b11c1a77e1659
Abhishek Sonje <code.abhishek03@gmail.com>
2025-08-25 11:56:19 +0530

    working on image upload

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,6 +1,50 @@
 import { db } from "@/lib/db";
 import { files } from "@/lib/db/schema";
 import { auth } from "@clerk/nextjs/server";
+import { unauthorized } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
 
+export async function POST(request: NextRequest) {
+  const { userId } = await auth();
 
+  try {
+    if (!userId) {
+      return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+    }
+    //PARSE BODY REQUEST
 
+    const body = await request.json();
+    const { imagekit, userId: bodyUserId } = body;
+
+    if (bodyUserId !== userId) {
+      return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+    }
+
+    if (!imagekit || !imagekit.url) {
+      return NextResponse.json(
+        { error: "Invalid file upload data" },
+        { status: 401 }
+      );
+    }
+
+    const fileData = {
+      name: imagekit.name || "untitled",
+      path: imagekit.filePath || `droply/${userId}/${imagekit.name}`,
+      size: imagekit.size || 0,
+      type: imagekit.fileType || "image",
+      fileUrl: imagekit.url,
+      thumbnailUrl: imagekit.thumbnailUrl || null,
+      userId: userId,
+      parentId: null, //Root level by default
+      isFolder: false,
+      isStarred: false,
+      isTrash: false,
+    };
+
+    const [newFile] = await db.insert(files).values(fileData).returning();
+    return NextResponse.json(newFile);
+  } catch (error) {
+
+    return NextResponse.json({ error: "Failed to save info to db" }, { status: 500 });
+  }
+}
